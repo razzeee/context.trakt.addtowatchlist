b5607066abbd73099c74f0d6c6db64a8e0c82d7f
Kolja Lampe <razzeee@gmail.com>
2020-06-16 00:51:47 +0200

    Add license and linebreak

--- addon.py
+++ addon.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-		
-		
-import xbmc		
-		
-if __name__ == '__main__':		
-	xbmc.executebuiltin("RunScript(script.trakt,action=addtowatchlist)")
+# -*- coding: utf-8 -*-
+
+import xbmc
+
+if __name__ == '__main__':
+    xbmc.executebuiltin("RunScript(script.trakt,action=addtowatchlist)")
